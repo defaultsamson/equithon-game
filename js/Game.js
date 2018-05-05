8486336c7d438893ae1046fbf68d91c6f5191c5b
Samson <stanmoon95@gmail.com>
2018-05-05 16:28:34 -0400

    Renamed Constants
    - Also fixed semicolons and formatting

--- js/Game.js
+++ js/Game.js
@@ -16,13 +16,10 @@ function gamePreload() {
     // Loading Images
     game.load.image("tiles", "assets/tiles.png");
     game.load.image("player", "assets/player.png");
-
     game.load.image("juicebox", "assets/juice.png"); //at
-
     game.load.image("healthbar", "assets/healthBar.png");
-    
     game.load.image("arrow", "assets/arrow.png");
-    
+
 
     /* // Spritesheet loading example
     this.load.spritesheet('dude',
@@ -40,13 +37,13 @@ var leftKey;
 var jumpKey;
 
 var glucoseBar;
-var glucoseTextPrefix = "Glucose Level (mg/dL): "
+var glucoseTextPrefix = "Glucose Level (mg/dL): ";
 var glucoseText;
 
-const worldWidth = 400;
-const worldHeight = 19;
-const blockWidth = 32;
-const blockHeight = 32;
+const WORLD_WIDTH = 400;
+const WORLD_HEIGHT = 19;
+const BLOCK_WIDTH = 32;
+const BLOCK_HEIGHT = 32;
 
 const glucoseBarX = 50;
 const glucoseBarY = 32;
@@ -72,12 +69,12 @@ function gameCreate() {
     map.addTilesetImage("tiles");
     map.setCollisionBetween(0, 999);
 
-    layer0 = map.create("layer0", worldWidth, worldHeight, blockWidth, blockHeight)
+    layer0 = map.create("layer0", WORLD_WIDTH, WORLD_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT);
     layer0.resizeWorld();
 
-    addMap("start")
-    addMap("house")
-    addMap("house")
+    addMap("start");
+    addMap("house");
+    addMap("house");
 
     player = game.add.sprite(40, 40, "player");
     player.scale.setTo(0.25, 0.25);
@@ -90,16 +87,18 @@ function gameCreate() {
     game.physics.enable(juicebox); //gives juicebox sprite a physics body at
     juicebox.body.allowGravity = false;
     juicebox.body.immovable = true;
-    
-    arrow=game.add.sprite(400, 0, "arrow"); //pointer on health bar
 
-    glucoseBar = game.add.sprite(glucoseBarX, glucoseBarY, "healthbar")
+    arrow = game.add.sprite(400, 0, "arrow"); //pointer on health bar
+
+    glucoseBar = game.add.sprite(glucoseBarX, glucoseBarY, "healthbar");
     glucoseBar.fixedToCamera = true;
     glucoseBar.width = 700;
     glucoseBar.height = 20;
 
-    var textStyle = {'font' : "16pt Comic Sans MS"}
-    glucoseText = game.add.text(300, 0, glucoseTextPrefix, textStyle)
+    var textStyle = {
+        'font': "16pt Comic Sans MS"
+    }
+    glucoseText = game.add.text(300, 0, glucoseTextPrefix, textStyle);
     glucoseText.fixedToCamera = true;
 
     // TODO
@@ -139,7 +138,7 @@ function addMap(toAdd) {
         } else {
             var y = 0;
             var yCoord;
-            while ((yCoord = y + yOffset) < worldHeight) {
+            while ((yCoord = y + yOffset) < WORLD_HEIGHT) {
                 var index = x + width * y;
                 // if the index is within the range of the array
                 if (index < width * height) {
@@ -153,7 +152,7 @@ function addMap(toAdd) {
                         var empty = true; // Assume that they are
                         var y2 = y + 1; // start searching from the next block
                         var yCoord2;
-                        while ((yCoord2 = y2 + yOffset) < worldHeight) {
+                        while ((yCoord2 = y2 + yOffset) < WORLD_HEIGHT) {
                             var index2 = x + width * y2;
                             if (index2 < width * height) {
                                 var id2 = data[x + width * y2] - 1;
@@ -166,7 +165,6 @@ function addMap(toAdd) {
                                 // below the file
                                 break;
                             }
-
                             y2++;
                         }
 
@@ -174,7 +172,7 @@ function addMap(toAdd) {
                             var y3 = y;
                             var yCoord3;
                             // replace all the following blocks with dirt
-                            while ((yCoord3 = y3 + yOffset) < worldHeight) {
+                            while ((yCoord3 = y3 + yOffset) < WORLD_HEIGHT) {
                                 map.putTile(DIRT_INDEX, x + xOffset, yCoord3);
                                 y3++;
                             }
@@ -185,18 +183,16 @@ function addMap(toAdd) {
                     // below the file, fill with dirty things
                     map.putTile(DIRT_INDEX, x + xOffset, yCoord);
                 }
-
                 y++;
             }
         }
     }
-
     xOffset += width;
 }
 
 function getYOffset(data, width, height) {
     var startHeight = 0;
-    for (var y = 0; y < worldHeight; y++) {
+    for (var y = 0; y < WORLD_HEIGHT; y++) {
         if (map.getTile(xOffset - 1, y)) {
             break;
         } else {
@@ -223,13 +219,13 @@ function jump() {
     player.body.velocity.y = -800;
 }
 
-const INCHING = 0.5
+const CAMERA_SPEED = 0.5
 var cameraOff = 0
 var touchingGround = false;
 
 function changeBloodSugar(degOfChange) {
     bloodSugar += degOfChange;
-    arrow.x +=10;
+    arrow.x += 10;
     juicebox.destroy();
     console.log(bloodSugar)
 }
@@ -243,20 +239,18 @@ var up = false;
 // Update game objects
 function gameUpdate() {
     // Makes the camera move to the left when the player pushes the viewport forward
-    cameraOff = Math.max(cameraOff + INCHING, player.x + 16 - (2 * WIDTH / 3));
+    cameraOff = Math.max(cameraOff + CAMERA_SPEED, player.x + 16 - (2 * WIDTH / 3));
     game.camera.x = cameraOff;
 
-    
-    
     // Maps controls to velocity
     if (rightKey.isDown && leftKey.isDown) {
         player.body.velocity.x = 0;
     } else if (rightKey.isDown) {
         player.body.velocity.x = 300;
-        arrow.x-=1
+        arrow.x -= 1;
     } else if (leftKey.isDown) {
         player.body.velocity.x = -300;
-        arrow.x-=1;
+        arrow.x -= 1;
     } else {
         player.body.velocity.x *= 0.75;
     }
@@ -276,13 +270,16 @@ function gameUpdate() {
     }
 
     // check if touching ground and handle collisions
-    this.game.physics.arcade.collide(player, layer0, (sprite, tile) => {if (sprite.body.onFloor()) {
-                                                                            touchingGround = true;
-                                                                        }
-                                                                    });
+    this.game.physics.arcade.collide(player, layer0, (sprite, tile) => {
+        if (sprite.body.onFloor()) {
+            touchingGround = true;
+        }
+    });
 
     //at - if collision happens between player and juicebox
-    this.game.physics.arcade.collide(player, juicebox, () => {changeBloodSugar(10)}); //check line 114
+    this.game.physics.arcade.collide(player, juicebox, () => {
+        changeBloodSugar(10);
+    }); //check line 114
     glucoseText.setText(glucoseTextPrefix + bloodSugar);
 }
 
