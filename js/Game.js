716d36024ce2bfa13e1f7f7a3516247a07b1ddad
Samson <stanmoon95@gmail.com>
2018-05-05 12:30:15 -0400

    Added yOffset to generated parts
    - Lines up the ground of the placed part with the start of the previous part

--- js/Game.js
+++ js/Game.js
@@ -11,6 +11,7 @@ function gamePreload() {
     // Loading tilemaps
     //game.load.tilemap("start", "assets/start.json", null, Phaser.Tilemap.TILED_JSON)
     game.load.json("start", "assets/start.json");
+    game.load.json("house", "assets/house.json");
 
     // Loading Images
     game.load.image("tiles", "assets/tiles.png");
@@ -29,6 +30,11 @@ var layer0;
 
 var rightKey;
 var leftKey;
+
+const worldWidth = 400;
+const worldHeight = 19;
+const blockWidth = 32;
+const blockHeight = 32;
 
 // Create game objects
 function gameCreate() {
@@ -50,15 +56,12 @@ function gameCreate() {
     map.addTilesetImage("tiles");
     map.setCollisionBetween(0, 999);
 
-    const worldWidth = 400;
-    const worldHeight = 19;
-    const blockWidth = 32;
-    const blockHeight = 32;
-
     layer0 = map.create("layer0", worldWidth, worldHeight, blockWidth, blockHeight)
     layer0.resizeWorld();
 
     addMap("start")
+    addMap("house")
+    addMap("house")
 
     player = game.add.sprite(40, 40, "player");
     game.physics.enable(player); // Gives player a physics body
@@ -81,19 +84,44 @@ var xOffset = 0;
 
 function addMap(toAdd) {
 
-    var taMap = game.cache.getJSON("start");
+    var taMap = game.cache.getJSON(toAdd);
     var data = taMap.layers[0].data;
     var width = taMap.layers[0].width;
     var height = taMap.layers[0].height;
 
+    var yOffset = getYOffset(data, width, height);
 
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
-            map.putTile(data[x + width * y] - 1, x + xOffset, y)
+            map.putTile(data[x + width * y] - 1, x + xOffset, y + yOffset);
         }
     }
 
     xOffset += width
+}
+
+function getYOffset(data, width, height) {
+    var startHeight = 0;
+    for (var y = 0; y < worldHeight; y++) {
+        if (map.getTile(xOffset - 1, y)) {
+            break;
+        } else {
+            startHeight++;
+        }
+    }
+
+    var offsetHeight = 0;
+    for (var y = 0; y < height; y++) {
+        // Empty tile
+        if (data[y * width] == 0) {
+            offsetHeight++
+        } else {
+            break;
+        }
+    }
+
+    // If xOffset == 0 then this is the first block, just place normally
+    return xOffset == 0 ? 0 : startHeight - offsetHeight;
 }
 
 function jump() {
@@ -108,7 +136,6 @@ function gameUpdate() {
     // Makes the camera move to the left when the player pushes the viewport forward
     cameraOff = Math.max(cameraOff + INCHING, player.x + 16 - (2 * WIDTH / 3));
     game.camera.x = cameraOff;
-
 
     // Maps controls to velocity
     if (rightKey.isDown) {
