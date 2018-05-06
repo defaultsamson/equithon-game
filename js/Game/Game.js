f82cb0d9c1ea84c21ffc75ebc4b91125b9c358c9
AnjaliDewdrop <anjali.dewdrop@gmail.com>
2018-05-06 10:07:52 -0400

    all done!

--- js/Game/Game.js
+++ js/Game/Game.js
@@ -297,7 +297,7 @@ function gameUpdate() {
             arrow.visible=false;
             
             console.log("omw to winning")
-            finish();        
+            //finish();        
         }
     }
 
