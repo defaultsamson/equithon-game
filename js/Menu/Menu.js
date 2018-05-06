21d77c794c0d937356e0b99227eb15bad127bd07
Samson <stanmoon95@gmail.com>
2018-05-06 03:31:49 -0400

    Finished Menu
    - Text might need rewriting

--- js/Menu/Menu.js
+++ js/Menu/Menu.js
@@ -8,6 +8,13 @@ var playButton;
 
 function menuPreload() {
     // Load images, resources, fonts, etc...
+    game.load.image("menuHealthbar", "assets/healthBar.png");
+    game.load.image("menuArrow", "assets/arrow.png");
+
+    game.load.spritesheet("keyboards", "assets/keyboards.png", 675, 135, 3);
+    game.load.spritesheet("menuMan", "assets/player.png", 105, 133, 2);
+
+    game.load.image("menuJuice", "assets/juice.png");
 }
 
 function menuCreate() {
@@ -55,10 +62,45 @@ function menuCreate() {
     storyText1.fixedToCamera = true;
     storyText1.visible = false;
 
-    storyText2 = game.add.text(110, 100, "We hope that in our efforts\nyou will take it upon yourself\n to learn a little more about those\ndiabetes and how you can help\nwho have it. Enjoy the game!", textStyle);
+    storyText2 = game.add.text(110, 100, "We hope that in our efforts\nyou will take it upon yourself\n to learn a little more about those\ndiabetes and how you can help\nwho have it.", textStyle);
     storyText2.fixedToCamera = true;
     storyText2.visible = false;
 
+    keyboards = game.add.sprite(63, 126, "keyboards");
+    keyboards.animations.add('flash', [0, 1, 2], 2, true);
+    keyboards.visible = false;
+
+    storyText3 = game.add.text(130, 110, "In this game you will play\nas a diabetic boy trying to\nmake his way from home to\nschool and must maintain your\nblood sugar levels.", textStyle);
+    storyText3.fixedToCamera = true;
+    storyText3.visible = false;
+
+    storyText4 = game.add.text(130, 110, "Collect juice containers to \nincrease blood sugar and keep\nmoving to decrease.\nDon't fall behind!", textStyle);
+    storyText4.fixedToCamera = true;
+    storyText4.visible = false;
+
+    storyText5 = game.add.text(90, 290, "You may use your preferred controls.", textStyle);
+    storyText5.fixedToCamera = true;
+    storyText5.visible = false;
+
+    menuBar = game.add.sprite(glucoseBarX, glucoseBarY, "menuHealthbar");
+    menuBar.fixedToCamera = true;
+    menuBar.width = 700;
+    menuBar.height = 20;
+    menuBar.visible = false;
+
+    menuArrow = game.add.sprite(400, 30, "menuArrow"); //pointer on health bar
+    menuArrow.fixedToCamera = true;
+    menuArrow.width = 20;
+    menuArrow.height = 40;
+    menuArrow.visible = false;
+
+    menuPlayer = game.add.sprite(120, 280, "menuMan");
+    menuPlayer.scale.setTo(0.7, 0.7);
+    menuPlayer.visible = false;
+
+    menuJuice = game.add.sprite(590, 280, "menuJuice");
+    menuJuice.scale.setTo(0.7, 0.7);
+    menuJuice.visible = false;
 }
 
 var stopFlashStart = false;
@@ -90,13 +132,38 @@ function continueMenu() {
             flash = false;
             break;
         case 2:
+            storyText3.visible = true;
             storyText2.visible = false;
+            menuBar.visible = true;
+            menuArrow.visible = true;
             flashStartTimer = Date.now();
             flashStart = 3500;
             flash = false;
             break;
+        case 3:
+            storyText4.visible = true;
+            storyText3.visible = false;
+            flashStartTimer = Date.now();
+            menuPlayer.visible = true;
+            menuJuice.visible = true;
+            flashStart = 3500;
+            flash = false;
+            break;
+        case 4:
+            storyText4.visible = false;
+            storyText5.visible = true;
+            keyboards.visible = true;
+            keyboards.animations.play("flash");
+            flashStartTimer = Date.now();
+            flashStart = 2500;
+            flash = false;
+            menuPlayer.visible = false;
+            menuJuice.visible = false;
+            break;
+        default:
+            gotoIngame();
+            break;
     }
-
 
     screen++;
 }
@@ -108,7 +175,7 @@ function gotoIngame() {
 function menuUpdate() {
     if (!stopFlashStart && Date.now() - flashStartTimer >= flashStart) {
         flash = true;
-        skipText.visible = true;
+        if (screen != 4) skipText.visible = true;
     }
 
     if (flash) {
