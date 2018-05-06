c1d58b3dcf4e332121fc981dd2714e1b7b5eba03
Samson <stanmoon95@gmail.com>
2018-05-05 20:05:07 -0400

    Added animations

--- js/Game.js
+++ js/Game.js
@@ -15,7 +15,6 @@ function gamePreload() {
 
     // Loading Images
     game.load.image("tiles", "assets/tiles.png");
-    game.load.image("player", "assets/player0.png");
     game.load.image("juicebox", "assets/juice.png"); //at
     game.load.image("healthbar", "assets/healthBar.png");
     game.load.image("arrow", "assets/arrow.png");
@@ -26,6 +25,8 @@ function gamePreload() {
     game.load.image("sky3", "assets/sky3.png");
     game.load.image("sky4", "assets/sky4.png");
 
+    // 105 by 133 images, 2 frames
+    game.load.spritesheet("player", "assets/player.png", 105, 133, 2);
 
     /* // Spritesheet loading example
     this.load.spritesheet('dude',
@@ -62,6 +63,7 @@ var sky2;
 var sky3;
 var sky4;
 
+const PLAYER_SCALE = 0.3;
 
     //spawn juice boxes
 function juiceSpawn() {
@@ -124,10 +126,12 @@ function gameCreate() {
     addMap("ruins");
 
     player = game.add.sprite(40, 40, "player");
-    player.scale.setTo(0.25, 0.25);
+    player.scale.setTo(PLAYER_SCALE, PLAYER_SCALE);
     game.physics.enable(player); // Gives player a physics body
     player.body.bounce.x = 0.05; // Slightly bouncy off wall
     player.body.collideWorldBounds = true; // Collide with the
+    player.animations.add('walk', [0, 1], 4, true);
+    player.anchor.setTo(0.5, 0.5);
 
     juicebox = game.add.sprite(500, 200, "juicebox"); //at help random spawning...
     juicebox.scale.setTo(0.5, 0.5);
@@ -307,14 +311,20 @@ function gameUpdate() {
     // Maps controls to velocity
     if (rightKey.isDown && leftKey.isDown) {
         player.body.velocity.x = 0;
+        player.animations.stop("walk");
     } else if (rightKey.isDown) {
         player.body.velocity.x = 300;
         bloodSugar -= energyMove;
+        player.animations.play("walk");
+        player.scale.x = PLAYER_SCALE;
     } else if (leftKey.isDown) {
         player.body.velocity.x = -300;
         bloodSugar -= energyMove;
+        player.animations.play("walk");
+        player.scale.x = -PLAYER_SCALE;
     } else {
         player.body.velocity.x *= 0.75;
+        player.animations.stop("walk");
     }
 
     // Jump controls
@@ -329,24 +339,22 @@ function gameUpdate() {
     }
 
     // check if touching ground and handle collisions
-<<<<<<< HEAD
     this.game.physics.arcade.collide(player, layer1, (sprite, tile) => {
         if (sprite.body.onFloor()) {
             touchingGround = true;
         }
     });
+}
+
+//spawn juice boxes
+function spawnJuice(){
     
-    //spawn juice boxes
-    function spawnJuice(){
-        
-=======
     this.game.physics.arcade.collide(player, layer1);
     if (player.body.onFloor()) {
         touchingGround = true;
     }
     else {
         touchingGround = false;
->>>>>>> da3753a20bf94cf66d564d262148a88b97800065
     }
 
     //at - if collision happens between player and juicebox
